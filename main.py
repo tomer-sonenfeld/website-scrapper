f73dd1d526cbce65b019ee91b64f9714a62e0e13
tomerson <tomerson@bgu.ac.il>
2023-04-08 03:51:20 +0300

    js

--- main.py
+++ main.py
@@ -2,8 +2,24 @@ import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 from website import Website
+from selenium import webdriver
 
 if __name__ == '__main__':
-    example_url = 'https://lightricks.com/'
+    example_url = 'https://rad.com/'
     example_website = Website(example_url)
-    print(example_website.does_word_exist('developer'))
+    print(example_website.does_word_exist('software'))
+
+    # create webdriver object
+    # driver = webdriver.Chorme()
+    #
+    # # enter keyword to search
+    # keyword = "geeksforgeeks"
+    #
+    # # get geeksforgeeks.org
+    # driver.get("https://www.geeksforgeeks.org/")
+    #
+    # # get elements
+    # elements = driver.find_elements_by_tag_name("h2")
+    #
+    # # print complete elements list
+    # print(element)
--- website.py
+++ website.py
@@ -5,10 +5,13 @@ import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse, urljoin
 from googletrans import Translator
+from selenium.common import StaleElementReferenceException
+from selenium.webdriver.common.by import By
 
 visited_pages=set()
 positions_found = False
 visited_links = set()
+driver= webdriver.Chrome()
 
 class Website:
     def __init__(self, url):
@@ -20,84 +23,77 @@ class Website:
 
     def does_word_exist(self, word):
         self.does_word_exist__main_page(word)
+        global driver
+        driver.quit()
         return positions_found
 
     def does_word_exist__main_page(self,word):
-        global positions_found
-        global visited_pages
-        global visited_links
-        print('searchin main page: ' + self.url)
-        headers = {'Accept-Language': 'en-US,en;q=0.5'}
-        response = requests.get(self.url, headers=headers)
-        content = response.content
-        soup = BeautifulSoup(content, 'html.parser')
+        print('searching main page: ' + self.url)
 
+        # driver = webdriver.Chrome(
+        global driver
+        driver.get(self.url)
+        driver.implicitly_wait(10)
 
-        links = soup.find_all('a')
 
         keywords = ['job', 'jobs', 'career', 'careers', 'opportunity', 'opportunities',
                     'position', 'positions', 'opening', 'openings', 'opening', 'openings',
                     'קריירה', 'משרה', 'משרות', 'תפקיד', 'תפקידים', 'פתיחה', 'פתיחות']
 
+        links = driver.find_elements(By.TAG_NAME, "a")
+
         for link in links:
             if positions_found:
-                break
-
-            link_url = link.get('href')
-            link_text = link.get_text()
-
-
-            if link is not None and link_url is not None:
-                if any(keyword in link_text.lower() for keyword in keywords):
-                    link_url = urljoin(self.url, link_url)
-                    sub_page = Website(link_url)
-                    if link_text not in visited_links:
-                        print('entering link: ' + link_text)
-                        visited_links.add(link_text)
-                        sub_page.does_word_exist__careers_page(word)
+                return
+            try:
+                link_url = link.get_attribute('href')
+                link_text = link.get_attribute('text')
+                if link is not None and link_url is not None:
+                    if any(keyword in link_text.lower() for keyword in keywords):
+                        sub_page = Website(link_url)
+                        if link_text not in visited_links:
+                            print('entering link: ' + link_text)
+                            visited_links.add(link_text)
+                            sub_page.does_word_exist__careers_page(word)
+            except StaleElementReferenceException:
+                continue
 
     def does_word_exist__careers_page(self,word):
         print('searching career page: ' + self.url)
-        global positions_found
-        global visited_pages
-        global visited_links
-        headers = {'Accept-Language': 'en-US,en;q=0.5'}
-        response = requests.get(self.url, headers=headers)
-        content = response.content
-        soup = BeautifulSoup(content, 'html.parser')
-        text = soup.get_text()
 
-        driver = webdriver.Chrome()
+        # driver = webdriver.Chrome()
+        global driver
         driver.get(self.url)
         driver.implicitly_wait(10)
 
         if word in driver.page_source:
+            print('found word: ' + word)
+            global positions_found
             positions_found = True
             return
 
-        if word in text.lower():
-            positions_found = True
-            return
 
-        keywords=['job','jobs', 'career','careers','opportunity','opportunities',
-                'position','positions','opening','openings','opening','openings',
-                  'קריירה','משרה','משרות','תפקיד','תפקידים','פתיחה','פתיחות']
+        keywords = ['job', 'jobs', 'career', 'careers', 'opportunity', 'opportunities',
+                    'position', 'positions', 'opening', 'openings', 'opening', 'openings',
+                    'קריירה', 'משרה', 'משרות', 'תפקיד', 'תפקידים', 'פתיחה', 'פתיחות']
 
-        links = soup.find_all('a')
+        links = driver.find_elements(By.TAG_NAME, "a")
+
         for link in links:
             if positions_found:
-                break
-            link_url = link.get('href')
-            link_text = link.get_text()
-
-            if link is not None and link_url is not None:
-                if any(keyword in link_text.lower() for keyword in keywords):
-                    link_url = urljoin(self.url, link_url)
-                    sub_page = SubWebsite(link_url)
-                    if link_text not in visited_links:
-                        print('entering link: ' + link_text)
-                        visited_links.add(link_text)
-                        sub_page.does_word_exist__careers_page(word)
+                return
+            try:
+                link_url = link.get_attribute('href')
+                link_text = link.get_attribute('text')
+                if link is not None and link_url is not None:
+                    if any(keyword in link_text.lower() for keyword in keywords):
+                        sub_page = Website(link_url)
+                        if link_text not in visited_links:
+                            print('entering link: ' + link_text)
+                            visited_links.add(link_text)
+                            sub_page.does_word_exist__careers_page(word)
+            except StaleElementReferenceException:
+                continue
 
     def __repr__(self):
         return f'Website: {self.name}, URL: {self.url}'
